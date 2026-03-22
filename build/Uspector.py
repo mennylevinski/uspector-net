c371cb1c9874e8d51242890717aa7eff17363b49
Menny Levinski <mennylevinski@secuditor.com>
2026-03-22 10:01:58 +0200

    Add files via upload

--- build/Uspector.py
+++ build/Uspector.py
@@ -607,7 +607,7 @@ def is_private_ip(ip: str) -> bool:
 
 # ======= Main =======
 if __name__ == "__main__":
-    ensure_console(f"Uspector Network Scanner {version}")
+    ensure_console(f"Uspector Network Scanner")
 
     log_level = logging.DEBUG if "--debug" in sys.argv else logging.INFO
     log_file = None
